cee9aab7bf26ff1e8c178e3c9b9eb11baf0e2b59
daetherius <danielportalesrosado@gmail.com>
2011-01-15 13:29:25 -0600

    Base

---
+++ application/libraries/MY_Controller.php
@@ -0,0 +1,11 @@
+<?php  if (! defined('BASEPATH')) exit('No direct script access allowed');
+class MY_Controller extends Controller {
+	function __construct(){
+		echo 'MY';
+		parent::Controller();
+	}
+}
+// END _Controller class
+
+/* End of file Controller.php */
+/* Location: ./system/libraries/Controller.php */
